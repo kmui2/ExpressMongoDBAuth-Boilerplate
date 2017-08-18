a748cea7464a2e53ab7f2c7a56c9e64318a9a433
Kevin Mui <kmui2@wisc.edu>
2017-08-18 16:26:22 -0500

    switched from var to const

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-var express = require('express')
-var app = express()
-var path = require("path");
-var bodyParser = require('body-parser');
+const express = require('express')
+const path = require("path");
+const bodyParser = require('body-parser');
+
+let app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.set('port', (process.env.PORT || 8000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 8000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (request, response) {
   response.sendFile(path.join(__dirname + '/public/index.html'));
 })
 
 app.listen(app.get('port'), function () {
-  console.log("Node app is running at http://localhost:" + app.get('port'))
+  console.log("Node app is running at http://localhost:" + app.get('port'));
 })
