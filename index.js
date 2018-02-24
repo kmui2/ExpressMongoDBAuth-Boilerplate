db98d0190f03450057f32461d7bef295a5002aa1
kmui2 <kmui2@wisc.edu>
2018-02-24 12:21:49 -0600

    added hello endpoint

--- index.js
+++ index.js
@@ -10,10 +10,14 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.set('port', (process.env.PORT || 8000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (request, response) {
-  response.sendFile(path.join(__dirname + '/public/index.html'));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 })
 
 app.listen(app.get('port'), function () {
   console.log("Node app is running at http://localhost:" + app.get('port'));
 })
+
+app.get('/hello', (req, res) => {
+  res.send({greeting: 'hello!'});
+});
